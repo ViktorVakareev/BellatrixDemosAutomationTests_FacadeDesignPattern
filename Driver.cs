07df06906c19a523cf87125e6ffa5b1ed3046c44
VIKTOR VAKAREEV <v.vakareev@gmail.com>
2021-08-27 13:48:56 +0300

    Add project files.

---
+++ Driver.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+
+namespace BellatrixDemosAutomationTests_FacadeDesignPattern
+{
+    public abstract class Driver
+    {
+        //public abstract void Start(Browser browser);
+        public abstract void Quit();
+        public abstract void GoToUrl(string url);
+        //public abstract Element FindElement(By locator);
+        //public abstract List<Element> FindElements(By locator);
+        public abstract void WaitForAjax();
+        public abstract void WaitUntilPageLoadsCompletely();
+    }
+}
